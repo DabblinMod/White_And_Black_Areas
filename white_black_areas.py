e15cd1d5c99dacdbb5bb2601c235a3537364c174
Samuel Kusi Amportey <92940110+DabblinMod@users.noreply.github.com>
2022-05-11 10:33:55 -0400

    Editing whole code (#1)
    Editing whole program

--- white_black_areas.py
+++ white_black_areas.py
@@ -3,47 +3,23 @@
 # Remember, board coloring starts with top left cell => WHITE
 # and then alternates with BLACK as in a usual chessboard.
 def white_black_areas(cs, rs):
-    # return a 2-element tuple, (total_white_area, total_black_area)
+   # return a 2-element tuple, (total_white_area, total_black_area)
     total_white_area=0
     total_black_area=0
     #you want to get the total area of both white and black
-    #to do this create two separate funcs to get each area
-    pass
-    total_white_area=do_white_area(cs,rs)
-    total_black_area=do_black_area(cs,rs)
+    #create separate lists for the total of the correct squares
+    r_odd, r_even=sum(rs[::2]), sum(rs[1::2])
+    c_odd, c_even=sum(cs[::2]), sum(cs[1::2])
+    print("Columns and Rows: ",(r_odd, r_even), " and ", (c_even, c_odd))
+    #print("Black Squares: ",(r_odd, c_odd))
+    #white squares: c_odd * r_odd + r_even * c_even
+    #black squares: c_odd * r_even + r_odd * c_even
+    print("White Squares total: ",(c_odd * r_odd) + (r_even * c_even))
+    print("Black Squares total: ",(c_odd * r_even) + (r_odd * c_even))
+    total_white_area=(c_odd * r_odd) + (r_even * c_even)
+    total_black_area=(c_odd * r_even) + (r_odd * c_even)
     check_area(total_white_area, total_black_area, cs, rs)
-        
     return total_white_area,total_black_area
-
-def do_white_area(cs, rs):
-    area=[]
-    #match white value columns to white row values
-    for c in range(len(cs)):
-        for r in range(len(rs)):
-            #if you get an odd column, multiple it to odd row values
-            #if you get an even column, multipy it to even row values.
-            if((c+1)%2==0 and (r+1)%2==0):
-                area.append(cs[c] * rs[r])
-            elif((c+1)%2 > 0 and (r+1)%2 > 0):
-                area.append(cs[c] * rs[r])
-    #print("White Area: ",area)
-    print("Sum: ", sum(area))
-    return sum(area)
-
-def do_black_area(cs, rs):
-    area=[]
-    #match black column values to black row values
-    for c in range(len(cs)):
-        for r in range(len(rs)):
-            #if you get an odd column, multiple it to even row values
-            #if you get an even column, multipy it to odd row values.
-            if((c+1)%2==0 and (r+1)%2>0):
-                area.append(cs[c] * rs[r])
-            elif((c+1)%2 > 0 and (r+1)%2 == 0):
-                area.append(cs[c] * rs[r])
-    #print("Black Area: ",area)
-    print("Sum: ", sum(area))
-    return sum(area)
 
 def check_area(white_area, black_area, cs, rs):
     print("Sum of cs and rs areas: ",sum(cs)*sum(rs))
